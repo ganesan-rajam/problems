4b2910a4d2c01fed0160c3a5a4611d723c4ce654
Ganesan Rajam <grajam@cisco.com>
2019-06-17 10:18:44 +0530

    Added solution for factorial problem

--- src/main/java/ExerciseMain.java
+++ src/main/java/ExerciseMain.java
@@ -1,16 +1,26 @@
+/**
+ *
+ * This class provides main method to run the solutions for problems.
+ *
+ * @author  Ganesan Rajam
+ */
 public class ExerciseMain {
 
-    public static void main(String args[])
-    {
+    public static void runFactorial() {
         Factorial factorial = new Factorial();
 
         try {
-            for (int i = 0; i < 17; i++) {
+            for (int i = 0; i < 14; i++) {
                 System.out.println("Factorial for : " + i + " is : " + factorial.factorial(i));
                 System.out.println("Factorial for : " + i + " is : " + factorial.factorialUsingRecursion(i));
             }
         } catch (Exception e) {
             System.out.println("Exception: " + e);
         }
+    }
+
+    public static void main(String args[])
+    {
+        runFactorial();
     }
 }
